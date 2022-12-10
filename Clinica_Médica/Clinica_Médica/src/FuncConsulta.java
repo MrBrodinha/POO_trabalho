cd54cf6949221c676e24ebf19ff197d28423e251
Emanuel Dias Pacheco <emanuel.dias.pacheco@gmail.com>
2022-12-10 02:17:31 +0000

    a

--- Clinica_Médica/Clinica_Médica/src/FuncConsulta.java
+++ Clinica_Médica/Clinica_Médica/src/FuncConsulta.java
@@ -9,32 +9,31 @@ public class FuncConsulta {
 	public static void criarC(ArrayList<Consulta> c, ArrayList<Utente> u, int ui, ArrayList<Profissional> p, int pi)
 			throws ConsultaInvalida {
 
-		System.out.println("Qual a data? Formato:yyyy-MM-dd HH:mm ");
+		System.out.println("Qual a data? Formato:yyyy-MM-dd HH:mm");
 		String nomeData = Ler.umaString();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 		LocalDateTime data = LocalDateTime.parse(nomeData, formatter);
-		LocalDateTime data1 = LocalDateTime.parse(nomeData, formatter);
-		
-		System.out.println(data.isEqual(data1));
 
 		for (int j = 0; j < p.get(pi).getConsultas().size(); j++) {
 			if (p.get(pi).getConsultas().get(j).getData().isEqual(data)) {
-				throw new ConsultaInvalida("Profissional já tem consulta marcada a essa hora");
+				throw new ConsultaInvalida("Profissional já tem consulta marcada a essa hora, escolha outra hora. Formato:yyyy-MM-dd HH:mm");
 			}
 		}
 
 		for (int j = 0; j < u.get(ui).getFT().getConsultas().size(); j++) {
 			if (u.get(ui).getFT().getConsultas().get(j).getData().isEqual(data)) {
-				throw new ConsultaInvalida("Utente já tem consulta marcada a essa hora");
+				throw new ConsultaInvalida("Utente já tem consulta marcada a essa hora, escolha outra hora. Formato:yyyy-MM-dd HH:mm");
 			}
 		}
-
+		
 		Consulta c1 = new Consulta(u.get(ui), p.get(pi), data);
 		c.add(c1);
 		u.get(ui).getFT().addConsulta(c1);
 		p.get(pi).addConsulta(c1);
 		System.out.println("Consulta criada com sucesso");
 		atualizarfileC(c);
+		FuncUtentes.atualizarfileU(u);
+		FuncProfissional.atualizarfileP(p);
 	}
 
 	public static void removerC(ArrayList<Consulta> c, ArrayList<Utente> u, int i, ArrayList<Profissional> p) {
